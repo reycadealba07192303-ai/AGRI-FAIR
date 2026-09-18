e658b82ea9c51ea54da35a38e30cc5c10267819a
Josh-Mrzn <marzanjoshua77@gmail.com>
2026-09-18 17:09:46 +0800

    Update lockfiles and generated Flutter plugin registrants

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_secure_storage_darwin
 import geocoding_darwin
 import geolocator_apple
 import package_info_plus
+import path_provider_foundation
 import sqflite_darwin
 import url_launcher_macos
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
